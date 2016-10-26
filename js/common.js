79641fb3f07b8aca935597bd7300792ddc0a5d30
Sinnerion <a.dergachow@gmail.com>
2016-10-26 16:04:20 +0300

    page-language changer added

--- js/common.js
+++ js/common.js
@@ -1,25 +1,47 @@
-$(document).ready(function(){
-    $('.header-nav').slideUp();
-    $('.header-login').hover(function(){
-        $('.header-nav').slideDown(50);
-    });
-    $('.header-nav').mouseleave(function(){
-        $('.header-nav').slideUp(40);
-    });
+$(document).ready(function () {
+  $('.header-nav').slideUp();
+  $('.page-language_list').slideUp();
 
-    $('.main-sidebar_more').click(function(){
-        $('.main-sidebar_counter-txt').toggle();
-        $('.main-sidebar').toggleClass('active-sidebar');
-        $('.main-sidebar li').toggleClass('active-sidebar');
-    });
+  // Header navs
+  $('.header-login').hover(function () {
+    $('.header-nav').slideDown(50);
+  });
+  $('.header-nav').mouseleave(function () {
+    $('.header-nav').slideUp(40);
+  });
+  $('.page-language_current').click(function () {
+    $('.page-language_list').slideDown(60);
+  });
+  $('.page-language_list').mouseleave(function () {
+    $('.page-language_list').slideUp(50);
+  });
 
-    $(document).scroll(function () {
-        var previousScroll = 0;
-        var currentScroll = $(this).scrollTop();
-        if (currentScroll > previousScroll) {
-            $('.main-sidebar').css('top', '10px');
-        } else {
-            $('.main-sidebar').css('top', '70px');
-        }
-    });
+  // Header language changer
+  $('.page-language_list a').click(function () {
+    $('.page-language_current').html($(this).html());
+  });
+
+  // Sidebar nav
+  $('.main-sidebar_more').click(function () {
+    $('.main-sidebar_counter-txt').toggle();
+    $('.main-sidebar').toggleClass('active-sidebar');
+    $('.main-sidebar li').toggleClass('active-sidebar');
+  });
+  $(document).scroll(function () {
+    var previousScroll = 0;
+    var currentScroll = $(this).scrollTop();
+    if ($(window).width() < 768) {
+      if (currentScroll > previousScroll) {
+        $('.main-sidebar_nav').css('top', '-90px');
+      } else {
+        $('.main-sidebar_nav').css('top', '60px');
+      }
+    } else {
+      if (currentScroll > previousScroll) {
+        $('.main-sidebar_nav').css('top', '-35px');
+      } else {
+        $('.main-sidebar_nav').css('top', '30px');
+      }
+    }
+  });
 });
