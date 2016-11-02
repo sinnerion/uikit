82b529b7e5cad772dba6c61ad6f18d0d1785c1b3
Sinnerion <a.dergachow@gmail.com>
2016-11-02 12:24:16 +0200

    chat-sidebar changed with uk-flex

--- app/js/common.js
+++ app/js/common.js
@@ -30,70 +30,23 @@ $(document).ready(function () {
 
   // Chat
   $('.chat-btn').click(function(){
-    if($(window).width() <= 600) {
-      $('.chat').css({
-        'visibility': 'visible',
-        'width': '35%'
-      });
+    $('.chat').css({
+      'visibility': 'visible',
+      'width': '30%'
+    });
+    $('.content-block').css('width', '70%');
       $('.chat_close-btn').css('left', '-71px');
-      $('.content-block').removeClass('uk-width-1-1').addClass('uk-width-1-2');
-    } else {
-      $('.chat').css({
-        'visibility': 'visible',
-        'width': '25%'
-      });
-      $('.chat_close-btn').css('left', '-71px');
-      $('.content-block').removeClass('uk-width-1-1').addClass('uk-width-2-3');
-    }
   });
   $('.chat_close-btn').click(function(){
-    if($(window).width() <= 600) {
-      $(this).css('left', '180');
-      $('.chat').css({
-        'width': '0',
-        'visibility': 'hidden'
-      });
-      $('.content-block').removeClass('uk-width-1-2').addClass('uk-width-1-1');
-    } else {
-      $(this).css('left', '180');
-      $('.chat').css({
-        'width': '0',
-        'visibility': 'hidden'
-      });
-      $('.content-block').removeClass('uk-width-2-3').addClass('uk-width-1-1');
-    }
-
+    $('.content-block').css('width', '100%');
+    $(this).css('left', '180');
+    $('.chat').css({
+      'width': '0',
+      'visibility': 'hidden',
+    });
   });
   //---- Chat custom scroll
   $(".chat-content").mCustomScrollbar({
     theme:"light"
   });
-
-  //$(document).scroll(function () {
-  //  var previousScroll = 0;
-  //  var currentScroll = $(this).scrollTop();
-  //  if ($(window).width() < 768) {
-  //    if (currentScroll > previousScroll) {
-  //      $('.main-sidebar_nav').css('top', '-250px');
-  //      $('.main-sidebar_more').css({
-  //        'position': 'absolute',
-  //        'top': '0',
-  //        'line-height': '34px'
-  //    });
-  //    } else {
-  //      $('.main-sidebar_nav').css('top', '60px');
-  //      $('.main-sidebar_more').css({
-  //        'position': 'static',
-  //        'line-height': '60px'
-  //        // 'top': '70px'
-  //      });
-  //    }
-  //  } else {
-  //    if (currentScroll > previousScroll) {
-  //      $('.main-sidebar_nav').css('top', '-35px');
-  //    } else {
-  //      $('.main-sidebar_nav').css('top', '30px');
-  //    }
-  //  }
-  //});
 });
